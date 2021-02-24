8537dfd2f310dfcf1bd3a4542dec70d39e9fb8eb
unknown <magdalena.gut@microsoft.wsei.edu.pl>
2021-02-24 15:32:07 +0100

    update layout

---
+++ src/components/Wallet.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class Wallet extends Component {
+  state = {
+    x: ["USD", "AUD", "SGD", "PHP", "EUR"],
+    y: "USD",
+    z: "",
+    convertTo: "EUR",
+    result: "",
+    date: ""
+  };
+
+  handleSelect = e => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value,
+        result: null
+      },
+      this.calculate
+    );
+  };
+
+  handleInput = e => {
+    this.setState(
+      {
+        amount: e.target.value,
+        result: null,
+        date: null
+      },
+      this.calculate
+    );
+  };
+
+  calculate = () => {
+    const amount = this.state.amount;
+    if (amount === isNaN) {
+      return;
+    } else {
+      fetch(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`)
+        .then(res => res.json())
+        .then(data => {
+          const date = data.date;
+          const result = (data.rates[this.state.convertTo] * amount).toFixed(4);
+          this.setState({
+            result,
+            date
+          });
+        });
+    }
+  };
+
+  handleSwap = e => {
+    const base = this.state.base;
+    const convertTo = this.state.convertTo;
+    e.preventDefault();
+    this.setState(
+      {
+        convertTo: base,
+        base: convertTo,
+        result: null
+      },
+      this.calculate
+    );
+  };
+  render() {
+    const { x, y, z } = this.state;
+
+    return (
+      <div></div>
+
+    );
+  }
+}
+
+export default Wallet;
+
+
