14771f4c3f63c7bcce82a42da0efc08eaab6b8ce
unknown <magdalena.gut@microsoft.wsei.edu.pl>
2021-03-10 14:25:01 +0100

    news

--- src/pages/News.js
+++ src/pages/News.js
@@ -59,21 +59,19 @@ class News extends Component {
         text:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur beatae expedita modi cumque aliquid assumenda molestias quisquam repellendus iusto recusandae soluta officia ea consequuntur animi laudantium eaque quo vel eligendi, dolore doloribus harum. Necessitatibus laudantium, dolorem ipsa vero magnam accusamus! Rerum odio molestias quos inventore excepturi quam accusantium totam saepe.",
       },
-      {
-        id: "9",
-        img: "https://picsum.photos/300/209",
-        title: "Sample title",
-        text:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur beatae expedita modi cumque aliquid assumenda molestias quisquam repellendus iusto recusandae soluta officia ea consequuntur animi laudantium eaque quo vel eligendi, dolore doloribus harum. Necessitatibus laudantium, dolorem ipsa vero magnam accusamus! Rerum odio molestias quos inventore excepturi quam accusantium totam saepe.",
-      },
     ],
   };
 
   render() {
     const news = this.state.newsData.map((item) => (
       <div className="news" key={item.id}>
-        <img src={item.img} alt="" />
-
+        
+          <div className="image">
+             
+            <img src={item.img} alt="" />
+        
+          </div>
+        
         <div className="article">
           <div className="title">{item.title}</div>
           <p className="text">{item.text}</p>
